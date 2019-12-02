1b92e35d0cc93b52934dec3e2a002112559a281f
KJ Kim <gencode.me@gmail.com>
2019-12-02 21:36:36 +0900

    remove daphne for now

--- runlocalserver.sh
+++ runlocalserver.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 # export DJANGO_DEBUG="Debug"
 # find . -name \*.pyc -delete
-# python manage.py runserver
-daphne workcloud.asgi:application
+python manage.py runserver
+# daphne workcloud.asgi:application
