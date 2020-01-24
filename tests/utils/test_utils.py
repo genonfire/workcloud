88004b943ec86c18b6dd3574859802dd0acdaab2
KJ Kim <gencode.me@gmail.com>
2020-01-24 15:36:47 +0900

    modify coverage setting

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -9,34 +9,39 @@ from utils.text import Text
 
 class UtilTest(TestCase):
     def test_set_const(self):
+        result = False
         try:
             Const.NAME_MAX_LENGTH = 0
-            assert False
         except BindError:
+            result = True
             self.log('Correctly raised BindError for Const.')
 
         try:
             Text.INVALID_TOKEN = 'Invalid Token'
-            assert False
         except BindError:
+            result = True
             self.log('Correctly raised BindError for Text.')
 
+        assert result
+
     def test_trace(self):
+        result = False
         if Debug.test_mode():
             if not Debug._trace_enabled():
                 settings.TRACE_ENABLED = True
             Debug.trace('Trace test.')
             Debug.error('Error test.')
             settings.TRACE_ENABLED = False
-        else:
-            assert False
+            result = True
+        assert result
 
     def test_log(self):
+        result = False
         if Debug.debug_or_test_mode():
             if not Debug.debug_mode():
                 settings.DEBUG = True
             Debug.log('log test.')
             Debug.callstack(limit=0)
             settings.DEBUG = False
-        else:
-            assert False
+            result = True
+        assert result
