a07acb838c10a3b5795a8281ff961985b030f100
KJ Kim <gencode.me@gmail.com>
2021-02-02 16:17:43 +0900

    Update response.py

--- core/response.py
+++ core/response.py
@@ -12,15 +12,6 @@ from rest_framework.status import (
 
 
 class Response(_Response):
-    HTTP_200 = HTTP_200_OK
-    HTTP_201 = HTTP_201_CREATED
-    HTTP_204 = HTTP_204_NO_CONTENT
-    HTTP_400 = HTTP_400_BAD_REQUEST
-    HTTP_401 = HTTP_401_UNAUTHORIZED
-    HTTP_403 = HTTP_403_FORBIDDEN
-    HTTP_404 = HTTP_404_NOT_FOUND
-    HTTP_501 = HTTP_501_NOT_IMPLEMENTED
-
     def __init__(self, data=None, status=None,
                  template_name=None, headers=None,
                  exception=False, content_type=None):
@@ -30,6 +21,15 @@ class Response(_Response):
         }
 
         super().__init__(response_data, status=status)
+        
+    HTTP_200 = HTTP_200_OK
+    HTTP_201 = HTTP_201_CREATED
+    HTTP_204 = HTTP_204_NO_CONTENT
+    HTTP_400 = HTTP_400_BAD_REQUEST
+    HTTP_401 = HTTP_401_UNAUTHORIZED
+    HTTP_403 = HTTP_403_FORBIDDEN
+    HTTP_404 = HTTP_404_NOT_FOUND
+    HTTP_501 = HTTP_501_NOT_IMPLEMENTED
 
 
 class PaginatedResponse(_Response):
