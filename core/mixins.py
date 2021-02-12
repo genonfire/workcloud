997174fc6a4446301f0ddf2ea3f1b36e40bbadb0
KJ Kim <gencode.me@gmail.com>
2021-02-12 13:19:41 +0900

    get_object to mixins

--- core/mixins.py
+++ core/mixins.py
@@ -1,4 +1,5 @@
 from core.response import Response
+from core.shortcuts import get_object_or_404
 from utils.debug import Debug
 
 
@@ -10,14 +11,24 @@ class ResponseMixin():
     Check rest_framework/mixins.py
     """
 
-    def _check_ownership(self, request, instance):
-        user = request.user
-        if user.is_staff:
-            return True
-        elif hasattr(instance, 'user') and user == instance.user:
-            return True
-        else:
-            return False
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (
+            'Expected view %s to be called with a URL keyword argument '
+            'named "%s". Fix your URL conf, or set the `.lookup_field` '
+            'attribute on the view correctly.' %
+            (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+        obj = get_object_or_404(queryset, **filter_kwargs)
+
+        self.check_object_permissions(self.request, obj)
+
+        return obj
 
     def set_serializer(self, serializer_class, *args, **kwargs):
         kwargs['context'] = self.get_serializer_context()
