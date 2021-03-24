20a507446cb9c78b8040d8d12efa2d942ed80ee6
KJ Kim <gencode.me@gmail.com>
2021-03-24 15:47:56 +0900

    introduce project initialization

--- README.md
+++ README.md
@@ -1,15 +1,47 @@
 # workcloud
 [![Build Status](https://travis-ci.com/genonfire/workcloud.svg?branch=master)](https://travis-ci.com/genonfire/workcloud) [![CircleCI](https://circleci.com/gh/genonfire/workcloud.svg?style=shield)](https://circleci.com/gh/genonfire/workcloud) [![codecov](https://codecov.io/gh/genonfire/workcloud/branch/master/graph/badge.svg)](https://codecov.io/gh/genonfire/workcloud)
 
-W.C. is workcloud
+Template for building web apps with Django REST framework + Vue.js
+
+
+# Create database before setup
+
+    $ psql
+    postgres=# create user <DB_USER>;
+    postgres=# alter user <DB_USER> with password '<DB_PASSWORD>';
+    postgres=# create database <DB_NAME> owner <DB_USER>;
 
 
 # Getting started with workcloud
 
-    $ git clone https://github.com/genonfire/workcloud.git
+    $ git clone https://github.com/genonfire/workcloud.git  # skip in case of template
     $ pip install -r requirements.txt
     $ cd frontend/wc
     $ npm install
+
+- It is highly suggested to start your project with workcloud template
+
+
+# Initialize project
+
+    $ python init.py
+    $ python manage.py migrate
+
+- This will replace the name of 'workcloud' in all directories and contents of files to your project name
+- This will create default secrets and config
+
+
+# Local Server
+
+    $ ./serve.sh
+    $ cd frontend/wc
+    $ npm run serve
+
+
+# unittest
+
+    $ tox  # You can use tox to generate coverage with unittest or just below script
+    $ ./runtest.sh  # This has more options for convenience. check more with ./runtest.sh --help
 
 
 # API Docs
--- init.py
+++ init.py
@@ -1,6 +1,9 @@
+import json
 import os
 import subprocess
 import sys
+
+from django.core.management.utils import get_random_secret_key
 
 
 REPLACE_DIRS = [
@@ -22,6 +25,29 @@ REPLACE_FILES = [
 IGNORE_FILES = [
     '.pyc',
 ]
+SECRETS_PATH = 'secrets.json'
+DEFAULT_SECRETS = {
+    "DB_ENGINE": "django.db.backends.postgresql",
+    "DB_HOST": "localhost",
+    "DB_PORT": "5432",
+    "DB_NAME": "",
+    "DB_USER": "",
+    "DB_PASSWORD": "",
+    "EMAIL_HOST": "",
+    "EMAIL_HOST_USER": "",
+    "EMAIL_HOST_PASSWORD": "",
+    "EMAIL_ADDRESS": "",
+    "AWS_ACCESS_KEY_ID": "",
+    "AWS_SECRET_ACCESS_KEY": "",
+    "SECRET_KEY": get_random_secret_key()
+}
+CONFIG_PATH = 'config.json'
+DEFAULT_CONFIG = {
+    "DEBUG": True,
+    "LOCAL_SERVER": True,
+    "TRACE_ENABLED": True,
+    "DO_NOT_SEND_EMAIL": True
+}
 
 
 def rename_directory(project_name):
@@ -49,7 +75,6 @@ def replace_project(project_name):
                 if not ignore:
                     filepath = os.path.join(dirname, filename)
                     with open(filepath) as f:
-                        print(filepath)
                         content = f.read()
                     content = content.replace('workcloud', project_name)
                     with open(filepath, 'w') as f:
@@ -64,3 +89,11 @@ if __name__ == '__main__':
     replace_project(project_name)
     rename_directory(project_name)
     subprocess.call(['sh', './trans.sh'])
+
+    if not os.path.isfile(SECRETS_PATH):
+        with open(SECRETS_PATH, 'w') as f:
+            f.write(json.dumps(DEFAULT_SECRETS, sort_keys=True, indent=4))
+
+    if not os.path.isfile(CONFIG_PATH):
+        with open(CONFIG_PATH, 'w') as f:
+            f.write(json.dumps(DEFAULT_CONFIG, sort_keys=True, indent=4))
