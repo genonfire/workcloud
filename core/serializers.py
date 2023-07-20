f8d49defecb2452de9d0a0c197aa71dfcd3969b9
KJ Kim <gencode.me@gmail.com>
2023-07-20 19:38:40 +0900

    Update serializers.py

--- core/serializers.py
+++ core/serializers.py
@@ -1,17 +1,28 @@
 from rest_framework import serializers
 
 
-class Serializer(serializers.Serializer):
+class SerialzierMixin():
+    def action(self):
+        return self.context.get('view').action
+
+    def getattr(self, attrs, field):
+        action = self.action()
+
+        if action == 'create':
+            return attrs.get(field)
+        elif action == 'partial_update':
+            return attrs.get(field, getattr(self.instance, field))
+        else:
+            raise AssertionError("not supported action.")
+
+
+class Serializer(SerialzierMixin, serializers.Serializer):
     pass
 
 
-class ListSerializer(serializers.ListSerializer):
+class ListSerializer(SerialzierMixin, serializers.ListSerializer):
     pass
 
 
-class ModelSerializer(serializers.ModelSerializer):
-    pass
-
-
-class HyperlinkedModelSerializer(serializers.HyperlinkedModelSerializer):
+class ModelSerializer(SerialzierMixin, serializers.ModelSerializer):
     pass
